ac656be13cb3d6fd643b66b2cbe551a4d658aed8
Nathan Trujillo <nathan.trujillo@intensity.com>
2017-01-10 19:02:57 -0800

    Remove buffertools

--- pixelpusher.js
+++ pixelpusher.js
@@ -3,8 +3,7 @@
 // Aaron Jones <aaron@inburst.io>
 //******************************************************************************
 
-var buffertools = require('buffertools'),
-    dgram       = require('dgram'),
+var dgram       = require('dgram'),
     Emitter     = require('events').EventEmitter,
     util        = require('util');
 
@@ -229,7 +228,7 @@ Controller.prototype.refresh = function(strips) {
         }
 
         // filter out sending dup data
-        if (that.currentStripData.length>0 && buffertools.equals(strips[i].data, that.currentStripData[i].data)) {
+        if (that.currentStripData.length>0 && strips[i].data.equals(that.currentStripData[i].data)) {
             continue;
         }
 
